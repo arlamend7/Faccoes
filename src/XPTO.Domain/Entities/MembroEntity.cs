553e5c04fae2c9283da1bd48fdc4c1491312c7ed
petrious.zamprogno <petrious@gmail.com>
2021-08-30 20:36:15 -0300

    finish entity membro.

--- src/XPTO.Domain/Entities/MembroEntity.cs
+++ src/XPTO.Domain/Entities/MembroEntity.cs
@@ -11,6 +11,20 @@ namespace XPTO.Domain.Entities
         public int IdMembro { get; set; }
         public int Faccao_idFaccao { get; set; }
         public FaccaoEntity Faccao { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataBatismo { get; set; }
+        public string NomeVulgo { get; set; }
+        public int Idade { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Referencia { get; set; }
+        public string Matricula { get; set; }
+        public byte[] Foto { get; set; }
+        public string CPF { get; set; }
+        public string NomeMae { get; set; }
+        public bool Obito  { get; set; }
+        public DateTime DataObito { get; set; }
+        public string LocalObito { get; set; }
+        public string Caracteristicas { get; set; }
 
     }
 }
